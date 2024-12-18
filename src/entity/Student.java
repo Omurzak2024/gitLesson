f71f053e13bad721569291a94f013f54a3f01fd7
Omurzak <soorbaevo@gmail.com>
2024-12-18 15:55:47 +0600

    bugFix Student class

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -6,5 +6,54 @@ public class Student {
     private Long id;
     private String firstName;
     private String lastName;
-    private LocalDate birthDay;
+    private LocalDate dateOfBirthDay;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirthDay() {
+        return dateOfBirthDay;
+    }
+
+    public void setDateOfBirthDay(LocalDate dateOfBirthDay) {
+        this.dateOfBirthDay = dateOfBirthDay;
+    }
+
+    public Student(Long id, String firstName, String lastName, LocalDate dateOfBirthDay) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirthDay = dateOfBirthDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirthDay=" + dateOfBirthDay +
+                '}';
+    }
 }
