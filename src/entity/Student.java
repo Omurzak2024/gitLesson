6a7beea3f3c0adc75e67f2bf66cbd691a3780237
Omurzak <soorbaevo@gmail.com>
2024-12-18 14:27:00 +0600

    Added class Student.java

---
+++ src/entity/Student.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+}
