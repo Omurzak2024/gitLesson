5aa8a1c8828d16125d0ad2e49cf4c8e397fc89cc
Omurzak <soorbaevo@gmail.com>
2024-12-18 15:38:56 +0600

    refactor Student class

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -7,4 +7,52 @@ public class Student {
     private String firstName;
     private String lastName;
     private LocalDate birthDay;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public Student(Long id, String firstName, String lastName, LocalDate birthDay) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                '}';
+    }
 }
